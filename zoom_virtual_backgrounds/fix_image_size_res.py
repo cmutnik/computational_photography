b8499744fae7cb9b58c1df4f143f552e35ab76f9
Corey Mutnik <cmutnik@hawaii.edu>
2020-08-14 20:49:13 -0400

    minor changes

--- zoom_virtual_backgrounds/fix_image_size_res.py
+++ zoom_virtual_backgrounds/fix_image_size_res.py
@@ -2,27 +2,28 @@
 '''
 from PIL import Image
 
-imfile='Cambodia.jpg'
-
+#imfile='Cambodia.jpg'
+imfile='20131114_154806.jpg'
 # open desired image
 img = Image.open(imfile)
-
 
 width, height = 1920, 1080
 
 img.thumbnail((width, height),Image.ANTIALIAS)
 
-img.save("cambodia_checkRES.jpg")
+img.save("cambodia_1920w_1080h.jpg")
 
 
 '''
 ########################################################
 from PIL import Image
-imfile='Cambodia.jpg'
+imfile='20131114_154806.jpg'
 # open desired image
 img = Image.open(imfile)
+#_, height = img.size
 # 768 is the height of the original image
-width, height = 1920, 768
+#width = 1920
+width, height = 1920, 1080
 # resize desired images
 img.thumbnail((width, height),Image.ANTIALIAS)
 # make black background of size from image that fits well
@@ -32,7 +33,7 @@ background = Image.new('RGB', (width, height))
 w,h=img.size
 background.paste(img, (int((width-w)/2),int((height-h)/2)))
 # save
-background.save("cambodia768h.jpg")
+background.save("cambodia_2.jpg")
 
 
 #width, height = 1920, 1080
