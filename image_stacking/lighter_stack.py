574e37fcd1af9b86558bf55b3103dd78f8bbf206
Greyson LaLonde <greyson.r.lalonde@wmich.edu>
2021-09-25 22:11:41 -1000

    formatting + reqs/port to py3

--- image_stacking/lighter_stack.py
+++ image_stacking/lighter_stack.py
@@ -1,11 +1,11 @@
-#http://onlyjus-photopy.blogspot.com/
+# http://onlyjus-photopy.blogspot.com/
 from PIL import Image, ImageChops
 import glob
 
-imgList = glob.glob('./*.JPG')
+imgList = glob.glob("./*.JPG")
 
 resultIm = Image.open(imgList[0])
-for i in range(1,len(imgList)):
+for i in range(1, len(imgList)):
     tempIm = Image.open(imgList[i])
     resultIm = ImageChops.lighter(resultIm, tempIm)
 
