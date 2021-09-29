3987a923c9aafc7e995f1c6296e415245dcf678f
Corey Mutnik <cmutnik@hawaii.edu>
2021-09-29 09:29:07 -1000

    (tako) updated boarders and thumbnails

--- boarders_and_thumbnails/addBackground.py
+++ boarders_and_thumbnails/addBackground.py
@@ -1,7 +1,8 @@
+#!/bin/python
 from PIL import Image
 
-inFilename = "figs/Obs_PS_lsum_aitoff_map.png"
-outFilename = "figs/out_addBackground.jpg"
+inFilename = "../figs/boarders_and_thumbnails/Obs_PS_lsum_aitoff_map.png"
+outFilename = "../figs/boarders_and_thumbnails/out_addBackground.jpg"
 
 img = Image.open(inFilename)
 x1, y1, x2, y2 = -10, -20, 1000, 1000  # cropping coordinates
